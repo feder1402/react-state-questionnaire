29c45752d48ddf3800085bed8cf88dd7d56062e4
Federico Raggi <federicoraggi@hotmail.com>
2022-07-05 16:06:41 -0700

    Report next events and if it is done

--- src/SchemaStateQuestionnaire.jsx
+++ src/SchemaStateQuestionnaire.jsx
@@ -6,13 +6,17 @@ import useQuestionnaireMachine from './useQuestionnaireMachine'
 import questionnaire from './questionnaire'
 
 const SchemaStateQuestionnaire = () => {
-  const [state, context, schema, send] = useQuestionnaireMachine(questionnaire)
+  const [state, context, schema, send, currentNode] = useQuestionnaireMachine(questionnaire)
 
   const title = schema.title
+  const nextEvents = currentNode.nextEvents;
+  const done = currentNode.done;
 
   return (
     <div>
       <h1>State: <span style={{color: "green"}}>{state}</span></h1>
+      <h1>Allowed Events: <span style={{color: "green"}}>{nextEvents}</span></h1>
+      <h1>Done?: <span style={{color: "green"}}>{done ? "Yes": "No"}</span></h1>
       <div style={{ display: 'flex', flex: '1 1 0', justifyItems: 'strech', height: '600px'}}>
         <Card title="Questionnaire">
           <Json json={questionnaire}/>
--- src/useQuestionnaireMachine.js
+++ src/useQuestionnaireMachine.js
@@ -7,8 +7,9 @@ const useQuestionnaireMachine = questionnaire => {
   // Extract schema and context from current state
   const schema = questionnaire.tasks.properties[state.value]
   const context = state.context
+  const currentNode = state
 
-  return [state.value, context, schema, send]
+  return [state.value, context, schema, send, currentNode]
 }
 
 export default useQuestionnaireMachine
